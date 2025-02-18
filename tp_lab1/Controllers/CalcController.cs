655949a51e9787204c59bec4c2c74eb904c7e3db
U <U@LAPTOP-8CU04DOR>
2025-02-18 22:10:22 +0400

    Добавьте файлы проекта.

---
+++ tp_lab1/Controllers/CalcController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Calculator.Models;
+
+public class CalcController : Controller
+{
+    public IActionResult Index()
+    {
+        return View(new Calculator());
+    }
+
+    [HttpGet]
+    public ViewResult RsvpForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult Calculate(Calculator calc, string action)
+    {
+        if (action == "clear")
+        {
+            ModelState.Clear();
+            return View("Index", new Calculator());
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View("Index", calc);
+        }
+
+        switch (calc.operation)
+        {
+            case "+":
+                calc.result = calc.operand1 + calc.operand2;
+                break;
+
+            case "-":
+                calc.result = calc.operand1 - calc.operand2;
+                break;
+
+            case "*":
+                calc.result = calc.operand1 * calc.operand2;
+                break;
+
+            case "/":
+                calc.result = calc.operand1 / calc.operand2;
+                break;
+        }
+
+        ViewBag.WantResult = 212;
+
+        string operationstr = $"{calc.operand1} {calc.operation} {calc.operand2} = {calc.result}";
+
+        HttpContext.Session.SetString("LastOperation", operationstr);
+
+        return View("Index", calc);
+    }
+    public IActionResult IndexLink()
+    {
+        ViewBag.LastOperation = HttpContext.Session.GetString("LastOperation");
+        return View("IndexLink");
+    }
+}
---
+++ tp_lab1/Models/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
+
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage ="Value is invalid!")]
+        public decimal operand1 { get; set; }
+        //[Compare("operand1", ErrorMessage ="Operands must not be equal!")]
+        [Required(ErrorMessage = "Value is invalid!")]
+        public sbyte operand2 { get; set; }
+        public decimal result { get; set; }
+        public string operation { get; set; }
+    }
+}
