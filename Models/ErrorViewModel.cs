039fca7394bac4b6d312e5bb511d272788eb8929
ksushka2 <159826761+ksushka2@users.noreply.github.com>
2025-05-05 00:19:24 +0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace laba6._1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
